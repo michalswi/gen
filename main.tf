977447eb3689d0187dd96159acdf6f16d5b7b792
ms <>
2024-10-15 12:01:34 +0200

    update

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ locals {
   }
 }
 
+
 resource "azurerm_resource_group" "kv_rg" {
   name     = "test"
   location = local.location
@@ -23,19 +24,27 @@ module "key_vault" {
   purge_protection_enabled = false
 }
 
-# todo - toremove
-resource "azurerm_virtual_network" "test" {
-  name                = "example-vnet"
-  location            = local.location
-  resource_group_name = azurerm_resource_group.kv_rg.name
-  address_space       = ["10.0.0.0/16"]
+
+resource "azurerm_resource_group" "vm_rg" {
+  name     = "testvm"
+  location = local.location
+  tags     = local.tags
 }
+
 # todo - toremove
-resource "azurerm_subnet" "test" {
-  name                              = "private-endpoint-subnet"
-  resource_group_name               = azurerm_resource_group.kv_rg.name
-  virtual_network_name              = azurerm_virtual_network.test.name
-  address_prefixes                  = ["10.0.1.0/24"]
+resource "azurerm_virtual_network" "vmtest" {
+  name                = "vm-vnet"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+# todo - toremove
+resource "azurerm_subnet" "vmtest" {
+  name                              = "vm-subnet"
+  resource_group_name               = azurerm_resource_group.vm_rg.name
+  virtual_network_name              = azurerm_virtual_network.vmtest.name
+  address_prefixes                  = ["10.10.1.0/24"]
   private_endpoint_network_policies = "Enabled"
 }
 
@@ -45,9 +54,9 @@ module "pe" {
   location = local.location
   tags     = local.tags
 
-  rg_name          = azurerm_resource_group.kv_rg.name
-  source_vnet_id   = azurerm_virtual_network.test.id
-  source_subnet_id = azurerm_subnet.test.id
+  rg_name          = azurerm_resource_group.vm_rg.name
+  source_vnet_id   = azurerm_virtual_network.vmtest.id
+  source_subnet_id = azurerm_subnet.vmtest.id
 
   priv_conn_config = {
     name_prefix                      = "keyvault"
@@ -56,4 +65,17 @@ module "pe" {
     subresource_names                = ["vault"]
     private_dns_zone_name            = "privatelink.vaultcore.azure.net"
   }
+}
+
+module "vm" {
+  source = "git::git@github.com:michalswi/vm.git?ref=main"
+
+  location = local.location
+  tags     = local.tags
+
+  rg_name          = azurerm_resource_group.vm_rg.name
+  source_subnet_id = azurerm_subnet.vmtest.id
+  source_vnet_name = azurerm_virtual_network.vmtest.name
+
+  ip_whitelist = ["<ip>"]
 }
