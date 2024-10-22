67488e17f5cbaa5a6c91c59b2b6d954ee8211aa0
ms <>
2024-10-22 12:51:19 +0200

    updates

--- main.tf
+++ main.tf
@@ -7,7 +7,51 @@ locals {
 }
 
 
-resource "azurerm_resource_group" "kv_rg" {
+## LA related
+
+module "log_analytics_name" {
+  source        = "git::git@github.com:michalswi/caf.git?ref=main"
+  resource_type = "azurerm_log_analytics_workspace"
+
+  # org_id        = var.org_id
+  # region        = var.region
+  # function      = var.function
+  # environment   = var.environment
+  index = "001"
+}
+
+resource "azurerm_resource_group" "rg_la" {
+  name     = "testla"
+  location = local.location
+  tags     = local.tags
+}
+
+module "log_analytics" {
+  source = "git::git@github.com:michalswi/la.git?ref=main"
+
+  name = module.log_analytics_name.caf_name
+
+  location          = local.location
+  rg_name           = azurerm_resource_group.rg_la.name
+  sku               = "PerGB2018"
+  retention_in_days = 30
+}
+
+
+## kv related
+
+module "key_vault_name" {
+  source = "git::git@github.com:michalswi/caf.git?ref=main"
+
+  # org_id        = var.org_id
+  # region        = var.region
+  # function      = var.function
+  # environment   = var.environment
+  index         = "001"
+  resource_type = "azurerm_key_vault"
+}
+
+resource "azurerm_resource_group" "rg_kv" {
   name     = "test"
   location = local.location
   tags     = local.tags
@@ -17,16 +61,89 @@ module "key_vault" {
   source = "git::git@github.com:michalswi/kv.git?ref=main"
 
   location = local.location
-  rg_name  = azurerm_resource_group.kv_rg.name
-  tags     = local.tags
+  rg_name  = azurerm_resource_group.rg_kv.name
+  name     = module.key_vault_name.caf_name
 
-  enable_logs              = false
+  tags = local.tags
+
+  # enable_logs = false
+  # 'enable_logs' by default 'true'
+  log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
+
   purge_protection_enabled = false
   retention_days           = 7
 }
 
 
-# VM related
+## AG related
+
+module "application_gateway_name" {
+  source        = "git::git@github.com:michalswi/caf.git?ref=main"
+  resource_type = "azurerm_application_gateway"
+
+  # org_id        = var.org_id
+  # region        = var.region
+  # function      = var.function
+  # environment   = var.environment
+  index = "001"
+}
+
+resource "azurerm_resource_group" "ag" {
+  name     = "ag-rg"
+  location = local.location
+  tags     = local.tags
+}
+
+resource "azurerm_virtual_network" "ag" {
+  name                = "ag-vnet"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.ag.name
+  address_space       = ["10.20.0.0/16"]
+}
+
+resource "azurerm_subnet" "ag" {
+  name                              = "ag-subnet"
+  resource_group_name               = azurerm_resource_group.ag.name
+  virtual_network_name              = azurerm_virtual_network.ag.name
+  address_prefixes                  = ["10.20.1.0/24"]
+  private_endpoint_network_policies = "Enabled"
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "ag-pip"
+  resource_group_name = azurerm_resource_group.ag.name
+  location            = local.location
+  sku                 = "Standard"
+  sku_tier            = "Regional"
+  allocation_method   = "Static"
+  ip_version          = "IPv4"
+}
+
+module "azure_app_gateway" {
+  source = "git::git@github.com:michalswi/ag.git?ref=main"
+
+  name          = module.application_gateway_name.caf_name
+  location      = local.location
+  backend_fqdns = ["myappservice.azurewebsites.net"]
+  rg_name       = azurerm_resource_group.ag.name
+  key_vault_id  = module.key_vault.key_vault_id
+
+  # enable_logs = false
+  log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
+
+  # if [] use 'default' cert
+  # certificate_refs = ["ag-ssl-cert","test-ssl-cert"]
+
+  agw_public_ip_id = azurerm_public_ip.pip.id
+
+  agw_subnet_address_prefixes = azurerm_subnet.ag.address_prefixes
+  agw_subnet_id               = azurerm_subnet.ag.id
+
+  tags = local.tags
+}
+
+
+## VM related
 # resource "azurerm_resource_group" "vm_rg" {
 #   name     = "testvm"
 #   location = local.location
@@ -82,66 +199,8 @@ module "key_vault" {
 #   ip_whitelist = ["<ip>"]
 # }
 
-# AG related
 
-resource "azurerm_resource_group" "ag" {
-  name     = "ag-rg"
-  location = local.location
-  tags     = local.tags
-}
-
-resource "azurerm_virtual_network" "ag" {
-  name                = "ag-vnet"
-  location            = local.location
-  resource_group_name = azurerm_resource_group.ag.name
-  address_space       = ["10.20.0.0/16"]
-}
-
-resource "azurerm_subnet" "ag" {
-  name                              = "ag-subnet"
-  resource_group_name               = azurerm_resource_group.ag.name
-  virtual_network_name              = azurerm_virtual_network.ag.name
-  address_prefixes                  = ["10.20.1.0/24"]
-  private_endpoint_network_policies = "Enabled"
-}
-
-resource "azurerm_public_ip" "pip" {
-  name                = "ag-pip"
-  resource_group_name = azurerm_resource_group.ag.name
-  location            = local.location
-  sku                 = "Standard"
-  sku_tier            = "Regional"
-  allocation_method   = "Static"
-  ip_version          = "IPv4"
-}
-
-module "azure_app_gateway" {
-  source = "git::git@github.com:michalswi/ag.git?ref=main"
-
-  location      = local.location
-  backend_fqdns = ["myappservice.azurewebsites.net"]
-  rg_name       = azurerm_resource_group.ag.name
-  key_vault_id  = module.key_vault.key_vault_id
-
-  enable_logs = false
-
-  # if [] use 'default' cert
-  # certificate_refs = ["ag-ssl-cert","test-ssl-cert"]
-
-  agw_public_ip_id = azurerm_public_ip.pip.id
-
-  agw_subnet_address_prefixes = azurerm_subnet.ag.address_prefixes
-  agw_subnet_id               = azurerm_subnet.ag.id
-
-  tags = local.tags
-}
-
-# module "log_analytics_workspace" {
-#   source   = "git::git@github.com:michalswi/la.git?ref=main"
-#   location = local.location
-#   rg_name  = azurerm_resource_group.la.name
-#   tags     = local.tags
-# }
+## SA related
 
 # module "storage_account" {
 #   source      = "git::git@github.com:michalswi/sa.git?ref=main"
